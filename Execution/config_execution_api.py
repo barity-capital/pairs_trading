9ab3d7d9f55605c6d564103d6a10cf7051143ea2
Quan Nguyen <minhquan0902@gmail.com>
2024-05-10 01:35:28 +1000

    update ticker

--- Execution/config_execution_api.py
+++ Execution/config_execution_api.py
@@ -19,24 +19,26 @@ from binance.client import Client
 
 # Config variables
 mode = "on"
-ticker_1 = "AGIXUSDT"
-ticker_2 = "OCEANUSDT" # OCEANUSDT
-signal_positive_ticker = ticker_2
-signal_negative_ticker = ticker_1
+ticker_1 = "OCEANUSDT"
+ticker_2 = "AGIXUSDT"  # OCEANUSDT
+signal_positive_ticker = ticker_1
+signal_negative_ticker = ticker_2
 rounding_ticker_1 = 2
 rounding_ticker_2 = 2
 quantity_rounding_ticker_1 = 0
 quantity_rounding_ticker_2 = 3
 
-limit_order_basis = False # Will ensure positions (expect for close) will be placed on limit basis
+# Will ensure positions (expect for close) will be placed on limit basis
+limit_order_basis = False
 
-tradeable_capital_usdt = 50 # Total tradeable capital to be split between both pairs # Test lower liquidity
-stop_loss_fail_safe = 0.15 # Stop loss at market order in case of drastic event
-signal_trigger_thresh = 0 # Z-score threshold to trigger signal (must above 0)
+# Total tradeable capital to be split between both pairs # Test lower liquidity
+tradeable_capital_usdt = 50
+stop_loss_fail_safe = 0.15  # Stop loss at market order in case of drastic event
+signal_trigger_thresh = 0  # Z-score threshold to trigger signal (must above 0)
 
-timeframe = "1h" # make sure matches your strategy timeframe
-kline_limit = 500 # make sure matches your strategy timeframe
-z_score_window = 21 # make sure matches your strategy timeframe
+timeframe = "1h"  # make sure matches your strategy timeframe
+kline_limit = 500  # make sure matches your strategy timeframe
+z_score_window = 21  # make sure matches your strategy timeframe
 
 # Live API
 api_key_mainnet = "X5YOXEAJAvbsy57O0RG9y1LZTtfJpHKtBSZu05iDDF1biOryTqp0RXHkIMuXZgcE"
